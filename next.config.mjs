e04f56ec2618fbbd7b2befe87a5126ce990d668f
Michael-Hunninen <Michaelhunninen@gmail.com>
2025-07-28 15:02:55 -0500

    Reset To Old Version

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,4 @@
 import { withPayload } from '@payloadcms/next/withPayload'
-import withPWA from 'next-pwa'
 
 const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
   ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
@@ -33,43 +32,6 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-// Configure PWA
-const pwaConfig = withPWA({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
-  runtimeCaching: [
-    {
-      urlPattern: /^https:\/\/fonts\.(googleapis|gstatic)\.com\/.*/i,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'google-fonts',
-        expiration: {
-          maxEntries: 4,
-          maxAgeSeconds: 365 * 24 * 60 * 60 // 365 days
-        }
-      }
-    },
-    {
-      urlPattern: /^\/api\/media\/.*/i,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'media-cache',
-        expiration: {
-          maxEntries: 100,
-          maxAgeSeconds: 30 * 24 * 60 * 60 // 30 days
-        }
-      }
-    },
-    {
-      urlPattern: /\.(js|css|html)$/i,
-      handler: 'StaleWhileRevalidate',
-      options: {
-        cacheName: 'static-resources'
-      }
-    }
-  ]
-})
+// PWA configuration removed
 
-export default withPayload(pwaConfig(nextConfig), { devBundleServerPackages: false })
+export default withPayload(nextConfig, { devBundleServerPackages: false })
